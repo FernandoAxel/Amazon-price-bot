362292ce3ae0349c079c02cf63ace6db4b196cf4
FernandoAxel <UTM20040053@utma.edu.mx>
2023-11-28 17:17:00 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import { getPrice } from "./src/scrapper.js";
+import { intro, outro, text, spinner } from "@clack/prompts";
+
+export default async function (){
+    const s = spinner();
+
+intro('welcome to amazon price bot!');
+
+const url = await text({
+    message: 'tType or paste your amazon URL: ',
+    placeholder: 'https://www.amazon.com.mx/dp/B0BRYVKW7F',
+    validate: (value) => {
+        if(!value.includes('www.amazon.com.mx')) return 'invalid amazon URL';
+    },
+});
+s.start('Getting price...');
+const price = await getPrice(url);
+s.stop(price);
+
+outro('Thanks for using my app!');
+}
